aeaf6758eed5943697ab7da6c9cbbff6fb653ebe
iio <hannes.forsgard@gmail.com>
2014-05-15 13:04:24 +0200

    Add Instantiator

---
+++ src/Instantiator.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details.
+ */
+
+namespace hanneskod\classtools;
+
+use ReflectionClass;
+use hanneskod\classtools\Exception\LogicException;
+
+/**
+ * Instantiate reflected class
+ *
+ * @author Hannes Forsgård <hannes.forsgard@fripost.org>
+ */
+class Instantiator
+{
+    /**
+     * @var ReflectionClass Reflected class to instantiate
+     */
+    private $class;
+
+    /**
+     * Set class to instantiate
+     *
+     * @param  ReflectionClass $class
+     * @return void
+     */
+    public function setReflectionClass(ReflectionClass $class)
+    {
+        $this->class = $class;
+    }
+
+    /**
+     * Get loaded reflection class
+     *
+     * @return ReflectionClass
+     * @throws LogicException If reflected class is not loaded
+     */
+    public function getReflectionClass()
+    {
+        if (!isset($this->class)) {
+            throw new LogicException("Reflected class not loaded");
+        }
+        return $this->class;
+    }
+
+    /**
+     * Get number of required constructor parameters
+     *
+     * @return int
+     */
+    public function countConstructorArgs()
+    {
+        if ($constructor = $this->class->getConstructor()) {
+            return $constructor->getNumberOfRequiredParameters();
+        }
+        return 0;
+    }
+
+    /**
+     * Check if class is instantiable
+     *
+     * @return boolean
+     */
+    public function isInstantiable()
+    {
+        return $this->getReflectionClass()->isInstantiable();
+    }
+
+    /**
+     * Check if class is instantiable without constructor parameters
+     *
+     * @return boolean
+     */
+    public function isInstantiableWithoutArgs()
+    {
+        return $this->isInstantiable() && !$this->countConstructorArgs();
+    }
+
+    /**
+     * Create instance
+     *
+     * @param  array $args Optional constructor arguments
+     * @return mixed Instance of reflected class
+     * @throws LogicException If reflected class is not instantiable
+     */
+    public function instantiate(array $args = null)
+    {
+        if (!$this->isInstantiable()) {
+            throw new LogicException("Reflected class is not instantiable");
+        }
+
+        $args = $args ?: [];
+
+        if (count($args) < $this->countConstructorArgs()) {
+            throw new LogicException("Unable to instantiate, too few constructor arguments");
+        }
+
+        return $this->getReflectionClass()->newInstanceArgs($args);
+    }
+}
---
+++ tests/InstantiatorTest.php
@@ -0,0 +1,43 @@
+<?php
+namespace hanneskod\classtools;
+
+class InstantiatorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testExceptionWhenReflectionClassNotSet()
+    {
+        $in = new Instantiator;
+        $this->setExpectedException('hanneskod\classtools\Exception\LogicException');
+        $in->getReflectionClass();
+    }
+
+    public function testIsInstantiable()
+    {
+        $in = new Instantiator;
+        $in->setReflectionClass(new \ReflectionClass('hanneskod\classtools\FilterableClassIterator'));
+        $this->assertTrue($in->isInstantiable());
+        $this->assertFalse($in->isInstantiableWithoutArgs());
+    }
+
+    public function testExceptionWhenInstantiatingNotInstatiable()
+    {
+        $in = new Instantiator;
+        $in->setReflectionClass(new \ReflectionClass('hanneskod\classtools\Exception'));
+        $this->setExpectedException('hanneskod\classtools\Exception\LogicException');
+        $in->instantiate();
+    }
+
+    public function testExceptionWhenInstantiatingWithToFewArgs()
+    {
+        $in = new Instantiator;
+        $in->setReflectionClass(new \ReflectionClass('hanneskod\classtools\FilterableClassIterator'));
+        $this->setExpectedException('hanneskod\classtools\Exception\LogicException');
+        $in->instantiate();
+    }
+
+    public function testInstantiate()
+    {
+        $in = new Instantiator;
+        $in->setReflectionClass(new \ReflectionClass('hanneskod\classtools\Instantiator'));
+        $this->assertInstanceOf('hanneskod\classtools\Instantiator', $in->instantiate());
+    }
+}
